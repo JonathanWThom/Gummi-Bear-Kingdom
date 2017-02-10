235932f9fbf879fb622c36555e8aa27a7d69e50b
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-02-09 19:35:45 -0800

    Show product page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,6 +18,14 @@ class ProductsController < ApplicationController
     end
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
 private
   def product_params
     params.require(:product).permit(:name, :cost, :country, :image)
