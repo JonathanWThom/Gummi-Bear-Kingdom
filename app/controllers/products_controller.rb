40205c417ab46b298a9ad832d6abbfa766da2ed3
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-02-09 19:29:40 -0800

    Create new product with flash notices

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,24 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Product Successfully Created"
+      redirect_to products_path
+    else
+      flash[:error] = "Please Try Again"
+      render :new
+    end
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :cost, :country, :image)
+  end
 end
